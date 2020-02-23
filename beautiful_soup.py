88a80f7e68f0736f2000678ceb7dc701dce13bc8
jieun <37922134+ji-eun-k@users.noreply.github.com>
2020-02-23 16:02:46 +0900

    FEB 23

--- beautiful_soup.py
+++ beautiful_soup.py
@@ -1,14 +1,27 @@
 from bs4 import BeautifulSoup
-ex1 = '''
-<html>
-    <head>
-        <title> HTML 연습 <title>
-    </head>
-    <body>
-        <p align="center"> text 1 </p>
-        <img src="c:\\temp\\image\\솔개.png">
-    </body>
-</html>'''
+from selenium import webdriver
+from selenium.webdriver.common.alert import Alert
+import time
 
-soup = BeautifulSoup(ex1, 'html.parser')
-txt = soup.find('p')
+query_txt = input('크롤링할 키워드는 무엇인가요? : ')
+
+path = "C:\\Users\\s_jeun097\\Desktop\\crawler\\chromedriver\\chromedriver.exe"
+driver = webdriver.Chrome(path)
+
+driver.get("https://korean.visitkorea.or.kr/main/main.do")
+time.sleep(3)
+
+dad = driver.find_element_by_id('safetyStay1')
+dad.find_element_by_class_name('btn_close3').click()
+
+time.sleep(2)
+
+driver.find_element_by_id("btnSearch").click()
+
+element = driver.find_element_by_id("inp_search")
+
+element.send.keys(query_txt)
+
+driver.find_element_by_link_text("검색").click()
+
+
